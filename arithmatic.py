e13d65652db4325bc5eb99170f3319769987cc75
chaitanyaduru2009-svg <chaitanyaduru2009@gmail.com>
2026-09-02 12:48:32 +0530

    Add files via upload

---
+++ arithmatic.py
@@ -0,0 +1,31 @@
+a=10
+b=40
+print("Addition:",a+b)
+print("subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a//b)
+print("Remainder:",a%b)
+print("Power:",a**b)
+#simple calculator
+a= int(input("Enter first number:"))
+b= int(input("Enter second number:"))
+print("Addition:",a+b)
+print("subtraction:",a-b)
+print("Multication:",a*b)
+#shopping bills calculation
+print1=float(input("Enter product 1 price:"))
+print2=float(input("Enter product 2 price:"))
+print3=float(input("Enter product 3 price:"))
+total=print1+print2+print3
+Discount=total*0.10
+print(total)
+final_amount=total-Discount
+print(final_amount)
+basic_salary=float(input("Enter your basic salary:"))
+HRA = basic_salary*0.12
+DA = basic_salary*0.30
+gross_salary = basic_salary+HRA+DA
+print(" basic_salary:",basic_salary)
+print("HRA:")
+print("DA:")
+print("gross_salary:")
