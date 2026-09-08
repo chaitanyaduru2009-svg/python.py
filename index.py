8d4fe418318dfeb373a92a33cfe76dbbd6a1a265
chaitanyaduru2009-svg <chaitanyaduru2009@gmail.com>
2026-09-08 15:03:56 +0530

    Add files via upload

--- index.py
+++ index.py
@@ -7,3 +7,32 @@ print(a<b)
 print(a>b)
 print(a<=b)
 print(a>=b)
+
+age=int(input("Enter your age:"))
+print("Eligible:", age>=18)
+
+marks=int(input("Enter your marks:"))
+print("passed:", marks>=45)
+
+correct_username="admin"
+correct_password="3456"
+username=input("Enter your username:")
+password=input("Enter your password:")
+print(username == correct_username)
+print(password == correct_password)
+
+age = 45
+citizen = True
+print( age>18 and citizen == True)
+
+tarun = True
+pardu = False
+print(tarun or pardu)
+
+karthik = False
+print(not karthik)
+
+marks = float(input("Enter your marks:"))
+attendence = float(input("Enter your attendence:"))
+eligible = marks>=56 and attendence>=76
+print("scholarship eligible:" , eligible)
