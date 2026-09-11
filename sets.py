b26788dacccf9eb2e754aebde238c553f445ce56
chaitanyaduru2009-svg <chaitanyaduru2009@gmail.com>
2026-09-11 08:52:58 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,21 @@
+#sets in python
+#set is a collection of unique values that is unodered
+#and mutable
+numbers={10,30,45,67}
+print(numbers)
+
+#students  have selected subj
+sub={"python","python","java","sql"}
+print(sub)
+
+#add value in sets
+sub={"eng","maths","history","eng"}
+sub.add("geo")
+print(sub)
+
+#remove in sets
+h={3,7,8,5}
+h.remove(8)
+print(h)
+
+#sets do not allow duplicate values it has only unique values
