4229ee728f37010504961c055bdf6534e22209ea
chaitanyaduru2009-svg <chaitanyaduru2009@gmail.com>
2026-09-11 08:52:21 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,18 @@
+#tuple in python
+#tuple is a collection of multiple values that is ordered
+# and unchangable
+h=("raghu",87,"python")
+print(h[2])
+
+#accesss values in tuple
+student=[34,65,76,87,98]
+print(student[2])
+
+#index method
+k=[54,76,87,87]
+print(k.index(54))
+
+numbers=[35,76,87,43]
+print(len(numbers))
+print(max(numbers))
+print(min(numbers))
