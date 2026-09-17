12b88c02d1026b0c36423b52e67bc74613cc9eb0
chaitanyaduru2009-svg <chaitanyaduru2009@gmail.com>
2026-09-17 20:52:11 +0530

    Add files via upload

---
+++ prob.py
@@ -0,0 +1,60 @@
+#display personal details using variables
+name=(input("Enter your name:"))
+age=int(input(("Enter your age:")))
+height=float(input(("Enter your height:")))
+print("name:")
+print("age:")
+print("height:")
+
+#personalized greetings
+name=input("Enter your name:")
+print("Hello,name:")
+
+#add two no. num reads as strings
+a=56
+b=56
+print(a+b)
+
+#float to integer conversion
+num=float(input("Enter float value:"))
+print(num)
+new=int(num)
+print(new)
+
+#sum using arthmetic operator
+a=int(input())
+b=int(input())
+print(a+b)
+
+#area of a rectangle
+length=float(input())
+breadth=float(input())
+area=length*breadth
+print(area)
+
+#Quotient and remainder
+a=int(input())
+b=int(input())
+q=a/b
+r=a%b
+print(q)
+print(r)
+
+#power and calculation
+base=int(input())
+exponent=int(input())
+print(base**exponent)
+
+#both pisitive no. check
+a=int(input("Enter first no."))
+b=int(input("Enter second no."))
+print(a>0 and b>0)
+
+#atleast one even no.
+n1=int(input("Enter first no."))
+n2=int(input("Enter second no."))
+print(n1%2==0 or n2%==0)
+
+#logical not a condition
+n1=int(input("Enter a number:"))
+print(not(n%2==0))
